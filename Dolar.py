70eccf3c639835e3db5c0cca23b26f09e244ae0c
jpdemasi <145072399+jpdemasi@users.noreply.github.com>
2025-01-14 16:26:51 -0300

    Update Dolar.py

--- Dolar.py
+++ Dolar.py
@@ -15,8 +15,8 @@ if response.status_code == 200:
     # Verificamos si se han encontrado al menos dos valores
     if len(values) >= 2:
         # Asignar los valores a las variables de venta y compra
-        venta = values[0].strip().replace('$', '').replace(',', '.')  # Elimina el '$' y cambiar la coma por un punto
-        compra = values[1].strip().replace('$', '').replace(',', '.')  # Elimina el '$' y cambiar la coma por un punto
+        compra = values[0].strip().replace('$', '').replace(',', '.')  # Elimina el '$' y cambiar la coma por un punto
+        venta = values[1].strip().replace('$', '').replace(',', '.')  # Elimina el '$' y cambiar la coma por un punto
 
         # Convertimos los valores a float si lo necesitas para operaciones matemáticas
         venta = float(venta)
